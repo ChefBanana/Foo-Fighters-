fa8276b9d73f99c540be00a62fdaa20df1784081
Zack Martin <zackmartin238@gmail.com>
2024-05-10 14:36:47 -0500

    commit2 (fixing this)

--- Characters/ZakkWylde/ZakkWylde.gd
+++ Characters/ZakkWylde/ZakkWylde.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var health = 12
+
+func _ready():
+	pass
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,6 +17,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if global_position.y > 900:
+		take_damage()
+		velocity.y = JUMP_VELOCITY
+		print("took damage")
+		print(health)
+	if health <= 0:
+		print("death")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -26,3 +38,15 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+func take_damage():
+	var health_bar = $"HUD/Health Bar/GreenHealthBar"
+	health -= 1
+	var health_percentage = health / 12.0
+	health_bar.size.x = health_percentage * 319
+	
+	
+
+
+	
+	
+	
