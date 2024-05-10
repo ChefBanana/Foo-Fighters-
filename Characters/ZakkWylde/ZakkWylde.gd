f50967f8bd5bd0b20824e0d02cdc137bf06446e8
ChefBanana <jonahrogers2143@gmail.com>
2024-05-10 14:56:43 -0500

    0.004
    Heck yeah, I'm actually doing something this time around

--- Characters/ZakkWylde/ZakkWylde.gd
+++ Characters/ZakkWylde/ZakkWylde.gd
@@ -1,11 +1,35 @@
 extends CharacterBody2D
 
 
+var Name : String = "Zakk Wylde"
+
+#Note: We'll change this later, since its useless right now,
+#but for now just leave player one true by default.
+
+var player1 : bool = true
+var player2 : bool = false
+var player3 : bool = false
+var player4 : bool = false
+
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+
+func _ready():
+	pass
+
+
+
+
+
+
+
+
 
 
 func _physics_process(delta):
---
+++ Scripts/StateMachine1.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var player1Name : String = ""
+var player2Name : String = ""
+var player3Name : String = ""
+var player4Name : String = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if $"../ZakkWylde".is_in_group("characters"):
+		pass # i'll change this, im just working on everything right now
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass#
